3afa28d2b297bd6cd5afe9c3715eb2a1f3c80f06
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-01-29 01:07:49 +0900

    웹 크롤링으로 가져온 데이터 값을 화면에 표시

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 function App() {
-  const [data, setDate] = useState("");
+  const [data, setDate] = useState([]);
 
   useEffect(() => {
     axios
-      .get("/api/data")
+      .get("/api/series")
       .then((res) => setDate(res.data))
       .catch((err) => console.log(err));
   }, []);
 
-  return <div>받아온 값 : {data}</div>;
+  return (
+      <div>
+        <h1>받아온 값:</h1>
+        <ul>
+          {data.map((title, index) => <li key={index}>{title}</li>)}
+        </ul>
+      </div>
+    );
 }
 
 export default App;
