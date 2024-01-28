91236b2d6c33f6a660fa18d52835e6406e1f6fd3
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-01-29 01:07:49 +0900

    웹 크롤링으로 가져온 데이터 값을 화면에 표시

--- src/main/java/com/example/contenthub/HubController.java
+++ src/main/java/com/example/contenthub/HubController.java
@@ -1,12 +1,15 @@
 package com.example.contenthub;
 
+import com.example.contenthub.crawling.CrawlingTest;
+import java.io.IOException;
+import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 class HubController {
-    @GetMapping("/api/data")
-    public String test() {
-        return "Hello, pingppung!";
+    @GetMapping("/api/series")
+    public List<String> test() throws IOException {
+        return CrawlingTest.crawl();
     }
 }
--- src/main/java/com/example/contenthub/crawling/CrawlingTest.java
+++ src/main/java/com/example/contenthub/crawling/CrawlingTest.java
@@ -10,29 +10,25 @@ import org.jsoup.select.Elements;
 
 public class CrawlingTest {
     final static String base_url = "https://series.naver.com/novel/top100List.series?rankingTypeCode=DAILY&categoryCode=ALL&page=";
-    public void crawl() throws IOException {
-        List<String> totalList = new ArrayList<>(); // 전체 데이터를 저장할 리스트
+    public static List<String> crawl() throws IOException {
+        List<String> novels = new ArrayList<>();
 
         for (int i = 1; i < 6; i++) {
             String url = base_url + i;
             Document doc = Jsoup.connect(url).get();
 
             List<String> list = getDataList(doc);
-            totalList.addAll(list); // 현재 페이지의 데이터를 전체 리스트에 추가
+            novels.addAll(list);
         }
-
-        // 전체 데이터 출력
-        for (String data : totalList) {
-            System.out.println(data);
-        }
+        return novels;
     }
 
-    private List<String> getDataList(Document document) {
+    private static List<String> getDataList(Document document) {
         List<String> list = new ArrayList<>();
         Elements selects = document.select(".comic_cont a[class*=NPI=a:content]");
 
         for (Element select : selects) {
-            list.add(select.html()); // 데이터를 리스트에 추가
+            list.add(select.html());
         }
         return list;
     }
