8c6668db47a72757ab22a75dc2962cf96e443ada
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-01-30 14:00:08 +0900

    소설 detail 페이지에서 장르 정보 추출

--- src/main/java/com/example/contenthub/crawling/NovelCrawler.java
+++ src/main/java/com/example/contenthub/crawling/NovelCrawler.java
@@ -10,13 +10,14 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class NovelCrawler {
-    final static String base_url = "https://series.naver.com/novel/top100List.series?rankingTypeCode=DAILY&categoryCode=ALL&page=";
+    final static String base_url = "https://series.naver.com";
+    final static String top100 = "/novel/top100List.series?rankingTypeCode=DAILY&categoryCode=ALL&page=";
 
     public static List<NovelData> crawl() throws IOException {
         List<NovelData> novels = new ArrayList<>();
 
         for (int i = 1; i < 6; i++) {
-            String url = base_url + i;
+            String url = base_url + top100 + i;
             Document doc = Jsoup.connect(url).get();
 
             List<NovelData> list = getDataList(doc);
@@ -25,15 +26,22 @@ public class NovelCrawler {
         return novels;
     }
 
-    private static List<NovelData> getDataList(Document document) {
+    private static List<NovelData> getDataList(Document document) throws IOException {
         List<NovelData> novels = new ArrayList<>();
         Elements novelElements = document.select(".comic_top_lst li");
 
         for (Element element : novelElements) {
-            String title = element.select(".comic_cont a[class*=NPI=a:content]").text();
+            Elements novel = element.select(".comic_cont a[class*=NPI=a:content]");
+            String title = novel.text();
+            String detailURL = base_url + novel.attr("href");
+
+            Document doc = Jsoup.connect(detailURL).get();
+            String genre = doc.select("li span a[href*=categoryTypeCode=genre]").text();
+
             String coverImg = element.select("img").attr("src");
             String summary = element.select(".comic_cont p[class*=dsc]").text();
-            NovelData novelData = new NovelData(title, coverImg, summary);
+
+            NovelData novelData = new NovelData(title, coverImg, summary, genre);
             novels.add(novelData);
         }
         return novels;
