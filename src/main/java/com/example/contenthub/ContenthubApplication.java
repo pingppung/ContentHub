afd47f383272be804a5f9abd718fb6e2f4e0dac7
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-02-01 02:38:45 +0900

    Spring Boot와 MongoDB를 연동하여 웹 크롤링 데이터 저장

--- src/main/java/com/example/contenthub/ContenthubApplication.java
+++ src/main/java/com/example/contenthub/ContenthubApplication.java
@@ -1,12 +1,13 @@
 package com.example.contenthub;
 
 import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ContenthubApplication {
-	public static void main(String[] args){
-		SpringApplication.run(ContenthubApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ContenthubApplication.class, args);
+    }
 }
--- src/main/java/com/example/contenthub/HubController.java
+++ src/main/java/com/example/contenthub/HubController.java
@@ -1,18 +1,27 @@
 package com.example.contenthub;
 
-import com.example.contenthub.crawling.NovelCrawler;
+import com.example.contenthub.service.NovelCrawlerService;
 
 import java.io.IOException;
 import java.util.List;
 
 import com.example.contenthub.crawling.NovelData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 class HubController {
+    private final NovelCrawlerService novelCrawlerService;
+
+    @Autowired
+    public HubController(NovelCrawlerService novelCrawlerService) {
+        this.novelCrawlerService = novelCrawlerService;
+    }
+
     @GetMapping("/novel")
     public List<NovelData> test() throws IOException {
-        return NovelCrawler.crawl();
+        novelCrawlerService.crawl();
+        return novelCrawlerService.getAllData();
     }
 }
