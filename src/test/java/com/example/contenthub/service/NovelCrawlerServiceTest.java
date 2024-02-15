5553e249c160d3ada3ced8e8fad07d59571a9b1e
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-02-15 23:12:06 +0900

    데이터베이스에 소설 저장 및 조회 테스트 추가

--- src/test/java/com/example/contenthub/service/NovelCrawlerServiceTest.java
+++ src/test/java/com/example/contenthub/service/NovelCrawlerServiceTest.java
@@ -5,6 +5,7 @@ import com.example.contenthub.crawling.novel.NaverSeriesCrawler;
 import com.example.contenthub.crawling.novel.NovelData;
 import com.example.contenthub.repository.NovelRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
@@ -15,6 +16,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static com.mongodb.assertions.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataMongoTest
@@ -30,19 +32,44 @@ public class NovelCrawlerServiceTest {
         novelCrawlerService = new NovelCrawlerService(novelRepository);
     }
 
+
+    @DisplayName("데이터베이스에 소설 저장 및 조회 테스트")
     @Test
-    public void testSaveNovels() {
+    void saveNovelsTest() {
+        NovelData novel1 = new NovelData("Title", "coverImg", "summary", "genre", Arrays.asList(new SiteDTO("Site1", "ID1")), false);
+
+        List<NovelData> list = new ArrayList<>();
+        list.add(novel1);
+
+        novelCrawlerService.saveNovels(list);
+        NovelData result = novelCrawlerService.getDataByTitle("Title");
+
+        assertEquals("Title", result.getTitle());
+        assertEquals("coverImg", result.getCoverImg());
+        assertEquals("summary", result.getSummary());
+        assertEquals("genre", result.getGenre());
+        assertEquals(1, result.getSite().size());
+        assertEquals("Site1", result.getSite().get(0).getSiteName());
+        assertEquals("ID1", result.getSite().get(0).getId());
+
+        deleteNovelTestDataByTitle("Title");
+    }
+
+    @DisplayName("같은 작품에 대해서 다른 사이트일 때 사이트 정보만 추가되는지 확인하는 테스트")
+    @Test
+    public void saveNovels_existingNovelTest() {
         NovelData existingNovel = new NovelData("Title", "coverImg", "summary", "genre", Arrays.asList(new SiteDTO("Site1", "ID1")), false);
         NovelData newNovel = new NovelData("Title", "coverImg", "summary", "genre", Arrays.asList(new SiteDTO("Site2", "ID2")), false);
-        List<NovelData> list = new ArrayList<>();
-        list.add(existingNovel);
-        list.add(newNovel);
-        novelCrawlerService.saveNovels(list);
-
+        List<NovelData> novels = List.of(existingNovel, newNovel);
+        novelCrawlerService.saveNovels(novels);
         NovelData result = novelCrawlerService.getDataByTitle("Title");
         assertEquals(2, result.getSite().size());
+        deleteNovelTestDataByTitle("Title");
+    }
 
-        //테스트 후에 테스트데이터 삭제
-        novelCrawlerService.deleteDataByTitle("Title");
+
+    //테스트 후에 테스트데이터 삭제
+    private void deleteNovelTestDataByTitle(String title) {
+        novelCrawlerService.deleteDataByTitle(title);
     }
 }
