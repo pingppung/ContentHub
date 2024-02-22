d73a3fc33b74d797ee21a9d0a11bdef49e586543
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-02-23 02:12:31 +0900

    네이버시리즈 크롤링 방식 Jsoup에서 Selenium으로 변경

--- src/main/java/com/example/contenthub/crawling/novel/NaverSeriesCrawler.java
+++ src/main/java/com/example/contenthub/crawling/novel/NaverSeriesCrawler.java
@@ -6,9 +6,15 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,50 +23,57 @@ import java.util.regex.Pattern;
 
 @Component
 public class NaverSeriesCrawler {
-    final static String base_url = "https://series.naver.com";
-    final static String top100 = "/novel/top100List.series?rankingTypeCode=HOURLY&categoryCode=ALL&page=";
+    final static String BASE_URL = "https://series.naver.com";
+    final static String TOP100 = "/novel/top100List.series?rankingTypeCode=DAILY&categoryCode=ALL&page=";
 
-    public List<NovelData> crawl() throws IOException {
+    private WebDriver createWebDriver() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/static/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        chromeOptions.addArguments("--headless");
+        return new ChromeDriver(chromeOptions);
+    }
+
+    private void closeWebDriver(WebDriver driver) {
+        driver.quit();
+    }
+
+    public List<NovelData> crawl() {
         List<NovelData> novels = new ArrayList<>();
-
-        for (int i = 1; i < 6; i++) {
-            String url = base_url + top100 + i;
-            Document doc = Jsoup.connect(url).get();
-
-            List<NovelData> list = getNaverSeriesData(doc);
-            novels.addAll(list);
+        WebDriver driver = createWebDriver();
+        try {
+            for (int i = 1; i < 6; i++) {
+                String url = BASE_URL + TOP100 + i;
+                driver.get(url);
+                Thread.sleep(2000);
+                List<NovelData> list = getNaverSeriesData(driver);
+                novels.addAll(list);
+            }
+        } catch (TimeoutException | InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            closeWebDriver(driver);
         }
         return novels;
     }
 
-    private List<NovelData> getNaverSeriesData(Document document) throws IOException {
+    private List<NovelData> getNaverSeriesData(WebDriver driver) throws InterruptedException {
         List<NovelData> novels = new ArrayList<>();
-        Elements novelElements = document.select(".comic_top_lst li");
+        List<WebElement> novelElements = driver.findElements(By.xpath("//*[@id=\"content\"]/div/ul/li"));
+        for (WebElement element : novelElements) {
+            WebElement novel = element.findElement(By.xpath(".//div[2]/h3/a"));
+            String title = extractTitle(novel.getText());
+            String detailHref = novel.getAttribute("href");
+            String productId = extractProductId(detailHref);
 
-        for (Element element : novelElements) {
-            Elements novel = element.select(".comic_cont a[class*=NPI=a:content]");
-            String title = extractTitle(novel.text());
-            String detailURL = base_url + novel.attr("href");
-            Pattern pattern = Pattern.compile("productNo=(\\d+)");
-            Matcher matcher = pattern.matcher(detailURL);
+            WebElement coverImgElement = element.findElement(By.xpath(".//a/img"));
+            String coverImg = coverImgElement.getAttribute("src");
+            String summary = element.findElement(By.xpath(".//div[2]/p[2]")).getText();
 
-            Document doc = Jsoup.connect(detailURL).get();
-            String genre = doc.select("li span a[href*=categoryTypeCode=genre]").text();
+            String genre = getNovelGenre(detailHref);
 
-            String coverImg = element.select("img").attr("src");
-            String summary = element.select(".comic_cont p[class*=dsc]").text();
+            boolean adultContent = isAdultContent(element);
 
-
-            String productId = null;
-            if (matcher.find()) {
-                productId = matcher.group(1);
-            } else {
-                System.out.println("해당 작품 id를 찾을 수가 없습니다!");
-            }
-            boolean adultContent = false;
-
-            Elements adultElements = element.select(".comic_cont h3 em[class*=ico n19]");
-            if (!adultElements.isEmpty()) adultContent = true;
             NovelData novelData = new NovelData(title, coverImg, summary, genre, Arrays.asList(new SiteDTO(NovelSite.NAVER_SERIES.getName(), productId)), adultContent);
 
             novels.add(novelData);
@@ -68,9 +81,46 @@ public class NaverSeriesCrawler {
         return novels;
     }
 
+    private String getNovelGenre(String href) {
+        WebDriver driver = createWebDriver();
+        try {
+            String detailURL = href;
+            driver.get(detailURL);
+
+            Thread.sleep(2000);
+            WebElement genreElement = driver.findElement(By.xpath("//*[@id=\"content\"]/ul[1]/li/ul/li[2]/span/a"));
+
+            return genreElement.getText();
+        } catch (TimeoutException | InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            closeWebDriver(driver);
+        }
+    }
+
+    private boolean isAdultContent(WebElement element) {
+        try {
+            element.findElement(By.xpath(".//div[2]/h3/em[@class='ico n19']"));
+            return true;
+        } catch (org.openqa.selenium.NoSuchElementException e) {
+            return false;
+        }
+    }
 
     // 정규표현식을 사용하여 [ ... ] 패턴을 찾아내고 해당 부분을 제거
     private static String extractTitle(String title) {
         return title.replaceAll("\\[.*?\\]", "").trim();
+    }
+
+    private String extractProductId(String input) {
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(input);
+
+        if (matcher.find()) {
+            return matcher.group();
+        } else {
+            System.out.println("해당 작품 id를 찾을 수가 없습니다!");
+            return null;
+        }
     }
 }
