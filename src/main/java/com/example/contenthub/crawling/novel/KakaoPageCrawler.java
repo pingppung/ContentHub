b81848513bb927e886d66e6fb37aa201f959f0c7
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-03-09 03:09:44 +0900

    crawler 코드 리팩토링 및 공통 예외처리 기능 추가

--- src/main/java/com/example/contenthub/crawling/novel/KakaoPageCrawler.java
+++ src/main/java/com/example/contenthub/crawling/novel/KakaoPageCrawler.java
@@ -1,8 +1,8 @@
 package com.example.contenthub.crawling.novel;
 
 import com.example.contenthub.crawling.SiteDTO;
+import com.example.contenthub.exception.CrawlerException;
 import com.example.contenthub.login.KakaoPageLogin;
-import com.example.contenthub.login.NaverSeriesLogin;
 import com.example.contenthub.service.NovelCrawlerService;
 import com.google.gson.Gson;
 import org.openqa.selenium.*;
@@ -28,27 +28,21 @@ public class KakaoPageCrawler {
     @Autowired
     @Lazy
     NovelCrawlerService novelCrawlerService;
-    final static String BASE_URL = "https://page.kakao.com";
-    final static String TOP300 = "/menu/10011/screen/94";
+    final static String BASE_URL = "https://page.kakao.com/menu/10011/screen/94";
 
     public List<NovelData> crawl() {
         List<NovelData> novels = new ArrayList<>();
         WebDriver driver = createWebDriver();
         WebDriver detailDriver = createWebDriver();
         try {
-            String url = BASE_URL + TOP300;
-            driver.get(url);
-            detailDriver.get(url);
-            kakakoLogin.activateBot(driver); //일단 커버이미지 때문에 로그인 상태로 만들어놓음
-            kakakoLogin.activateBot(detailDriver);
-
+            setUpDrivers(driver, detailDriver);
             scrollPageToBottom(driver);
-
-
             List<NovelData> list = getKakaoPageData(driver, detailDriver);
             novels.addAll(list);
+        } catch (TimeoutException e) {
+            throw new CrawlerException(CrawlerException.ExceptionMessage.TIMEOUT_EXCEPTION.getMessage(), e);
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            throw new CrawlerException(CrawlerException.ExceptionMessage.UNEXPECTED_EXCEPTION.getMessage(), e);
         } finally {
             closeWebDriver(driver);
             closeWebDriver(detailDriver);
@@ -58,46 +52,11 @@ public class KakaoPageCrawler {
         return novels;
     }
 
-    private List<NovelData> getKakaoPageData(WebDriver driver, WebDriver detailDriver) {
-        List<NovelData> novels = new ArrayList<>();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        List<WebElement> novelElements = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//*[@id=\"__next\"]/div/div[2]/div/div[2]/div[3]/div/div[2]/div/div/div/div")));
-        //List<WebElement> novelElements = driver.findElements(By.xpath("//*[@id=\"__next\"]/div/div[2]/div/div[2]/div[3]/div/div[2]/div/div/div/div"));
-        for (WebElement element : novelElements) {
-
-            WebElement novel = element.findElement(By.xpath(".//div/a/div"));
-            String jsonString = novel.getAttribute("data-t-obj");
-            Gson gson = new Gson();
-            Map<String, Object> data = gson.fromJson(jsonString, Map.class);
-            Map<String, String> eventMeta = (Map<String, String>) data.get("eventMeta");
-
-            String productId = eventMeta.get("id");
-            String title = eventMeta.get("name");
-
-            System.out.println(title);
-            if (novelCrawlerService.isDataExist(title, NovelSite.KAKAO_PAGE.getName())) continue;
-
-            String genre = eventMeta.get("subcategory");
-
-            WebElement imgDiv = novel.findElement(By.xpath(".//div/div/img"));
-            String coverImg = imgDiv.getAttribute("src");
-
-            boolean adultContent = isAdultContent(element);
-            String summary = getNovelSummary(detailDriver, productId);
-
-            NovelData novelData = new NovelData(title, coverImg, summary, genre, Arrays.asList(new SiteDTO(NovelSite.KAKAO_PAGE.getName(), productId)), adultContent);
-            novels.add(novelData);
-        }
-        return novels;
-    }
-
-    private boolean isAdultContent(WebElement element) {
-        try {
-            element.findElement(By.xpath(".//img[@alt='19세 뱃지']"));
-            return true;
-        } catch (org.openqa.selenium.NoSuchElementException e) {
-            return false;
-        }
+    private void setUpDrivers(WebDriver driver, WebDriver detailDriver) {
+        driver.get(BASE_URL);
+        detailDriver.get(BASE_URL);
+        kakakoLogin.activateBot(driver);
+        kakakoLogin.activateBot(detailDriver);
     }
 
     private void scrollPageToBottom(WebDriver driver) throws InterruptedException {
@@ -115,8 +74,50 @@ public class KakaoPageCrawler {
         }
     }
 
-    private String getNovelSummary(WebDriver driver, String productId) {
+    private List<NovelData> getKakaoPageData(WebDriver driver, WebDriver detailDriver) {
+        List<NovelData> novels = new ArrayList<>();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        List<WebElement> novelElements = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//*[@id=\"__next\"]/div/div[2]/div/div[2]/div[3]/div/div[2]/div/div/div/div")));
+        for (WebElement element : novelElements) {
+            NovelData novelData = extractNovelData(element, detailDriver);
+            if (novelData != null) novels.add(novelData);
+        }
+        return novels;
+    }
 
+    private NovelData extractNovelData(WebElement element, WebDriver detailDriver) {
+        WebElement novel = element.findElement(By.xpath(".//div/a/div"));
+        String jsonString = novel.getAttribute("data-t-obj");
+        Gson gson = new Gson();
+        Map<String, Object> data = gson.fromJson(jsonString, Map.class);
+        Map<String, String> eventMeta = (Map<String, String>) data.get("eventMeta");
+
+        String productId = eventMeta.get("id");
+        String title = eventMeta.get("name");
+
+        if (novelCrawlerService.isDataExist(title, NovelSite.KAKAO_PAGE.getName())) return null;
+
+        String genre = eventMeta.get("subcategory");
+        WebElement imgDiv = novel.findElement(By.xpath(".//div/div/img"));
+        String coverImg = imgDiv.getAttribute("src");
+        boolean adultContent = isAdultContent(element);
+        String summary = getNovelSummary(detailDriver, productId);
+
+        return new NovelData(title, coverImg, summary, genre,
+                Arrays.asList(new SiteDTO(NovelSite.KAKAO_PAGE.getName(), productId)), adultContent);
+    }
+
+    private boolean isAdultContent(WebElement element) {
+        try {
+            element.findElement(By.xpath(".//img[@alt='19세 뱃지']"));
+            return true;
+        } catch (org.openqa.selenium.NoSuchElementException e) {
+            return false;
+        }
+    }
+
+
+    private String getNovelSummary(WebDriver driver, String productId) {
         try {
             String detailURL = String.format("%s/content/%s?tab_type=about", BASE_URL, productId);
             driver.get(detailURL);
@@ -125,13 +126,9 @@ public class KakaoPageCrawler {
             By summaryLocator = By.xpath("//*[@id=\"__next\"]/div/div[2]/div[1]/div[2]/div[2]/div/div/div[1]/div/div[2]/div/div/span");
             WebElement summary = wait.until(ExpectedConditions.visibilityOfElementLocated(summaryLocator));
 
-//            //By summaryLocator = By.xpath("");
-//            waitForElement(driver, summaryLocator);
-//            WebElement summary = driver.findElement(summaryLocator);
-
             return summary.getText();
         } catch (TimeoutException e) {
-            throw new RuntimeException(e);
+            throw new CrawlerException(CrawlerException.ExceptionMessage.TIMEOUT_EXCEPTION.getMessage(), e);
         }
     }
 }
