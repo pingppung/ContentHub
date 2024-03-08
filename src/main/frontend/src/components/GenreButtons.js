35b5861c0b70507606873a40bd18d1bafdb52ef8
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-03-08 19:49:20 +0900

    NovelDetail 컴포넌트의 스크롤바 css 수정

--- src/main/frontend/src/components/GenreButtons.js
+++ src/main/frontend/src/components/GenreButtons.js
@@ -2,7 +2,7 @@ import React from "react";
 
 
 function GenreButtons({ selectedGenre, handleGenreClick }) {
-  const genres = ["전체", "로맨스", "로판", "판타지", "현판", "무협", "미스터리", "라이트노벨", "BL"];
+  const genres = ["전체", "로맨스", "로판", "판타지", "현판", "무협", "미스터리", "라이트노벨", "BL", "드라마"];
 
   return (
     <div className={"genreButtons"}>
--- src/main/frontend/src/enum/NovelSite.js
+++ src/main/frontend/src/enum/NovelSite.js
@@ -1,5 +1,6 @@
 const NovelSite = {
   NAVER_SERIES: { name: '네이버 시리즈', baseUrl: 'https://series.naver.com/novel/detail.series?productNo=' },
-  RIDI_BOOKS: { name: '리디북스', baseUrl: 'https://www.ridibooks.com' },
+  KAKAO_PAGE: { name: '카카오페이지', baseUrl: 'https://page.kakao.com/content/'},
+  RIDI_BOOKS: { name: '리디북스', baseUrl: 'https://www.ridibooks.com/' },
 };
 export default NovelSite;
