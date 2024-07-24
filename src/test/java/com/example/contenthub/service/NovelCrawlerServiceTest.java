6530753c97a359c278c77b7c817afa090a46313c
Pingppung <57535999+pingppung@users.noreply.github.com>
2024-07-24 14:46:09 +0900

    refactor: 테스트 코드 Mockito방식으로 변경

--- src/test/java/com/example/contenthub/service/NovelCrawlerServiceTest.java
+++ src/test/java/com/example/contenthub/service/NovelCrawlerServiceTest.java
@@ -1,22 +1,30 @@
 package com.example.contenthub.service;
 
-import com.example.contenthub.dto.ContentDTO;
-import com.example.contenthub.entity.Novel;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.example.contenthub.dto.ContentDTO;
+import com.example.contenthub.entity.Novel;
+import com.example.contenthub.repository.NovelRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class NovelCrawlerServiceTest {
 
-    @Autowired
+    @Mock
+    private NovelRepository novelRepository;
+
+    @InjectMocks
     private NovelCrawlerService novelCrawlerService;
 
     @DisplayName("데이터베이스에 소설 저장 및 조회 테스트")
@@ -30,6 +38,7 @@ public class NovelCrawlerServiceTest {
         novelCrawlerService.saveNovels(list, "네이버시리즈");
         Novel result = novelCrawlerService.getDataByTitle("Title");
 
+        // 적절한 Mockito 검증 로직 추가
         assertEquals("Title", result.getTitle());
         assertEquals("coverImg", result.getCoverImg());
         assertEquals("summary", result.getSummary());
@@ -52,9 +61,9 @@ public class NovelCrawlerServiceTest {
         deleteNovelTestDataByTitle("Title");
     }
 
-
-    //테스트 후에 테스트데이터 삭제
+    // 테스트 후에 테스트 데이터 삭제
     private void deleteNovelTestDataByTitle(String title) {
         novelCrawlerService.deleteDataByTitle(title);
     }
+
 }
